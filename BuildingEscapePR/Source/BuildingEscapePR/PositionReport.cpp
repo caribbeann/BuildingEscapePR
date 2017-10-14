720b1784abfdc8865fcc84ea6d7b8cce297a9f59
Stefan Oancea <oancea.stefan@icloud.com>
2017-10-15 00:49:05 +0200

    o piatra o usa deschisa si un scaun (@brigitte)

--- BuildingEscapePR/Source/BuildingEscapePR/PositionReport.cpp
+++ BuildingEscapePR/Source/BuildingEscapePR/PositionReport.cpp
@@ -1,7 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "PositionReport.h"
-
+#include "GameFramework/Actor.h"
 
 // Sets default values for this component's properties
 UPositionReport::UPositionReport()
@@ -18,8 +18,10 @@ UPositionReport::UPositionReport()
 void UPositionReport::BeginPlay()
 {
 	Super::BeginPlay();
-
-	// ...
+	
+	FString objName = GetOwner()->GetName();
+	FString objPos = GetOwner()->GetTransform().GetLocation().ToString();
+	UE_LOG(LogTemp, Warning, TEXT("%s is at %s!"), *objName, *objPos);
 	
 }
 
