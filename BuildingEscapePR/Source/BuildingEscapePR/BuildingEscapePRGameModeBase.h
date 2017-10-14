948ef64248edaf49d95f75fcbe0df625f5f6b3c8
Stefan Oancea <oancea.stefan@icloud.com>
2017-10-14 22:07:52 +0200

    Initial setup commint

---
+++ BuildingEscapePR/Source/BuildingEscapePR/BuildingEscapePRGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BuildingEscapePRGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BUILDINGESCAPEPR_API ABuildingEscapePRGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
