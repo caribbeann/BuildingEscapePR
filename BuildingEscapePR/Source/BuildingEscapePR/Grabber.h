b0fc2b846af6bbb40735168cf349f9f834e2bb6a
Stefan Oancea <oancea.stefan@icloud.com>
2017-10-15 21:34:10 +0200

    no message

--- BuildingEscapePR/Source/BuildingEscapePR/Grabber.h
+++ BuildingEscapePR/Source/BuildingEscapePR/Grabber.h
@@ -2,48 +2,28 @@
 
 #pragma once
 
-#include "Engine/TriggerVolume.h"
 #include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
 #include "GameFramework/Actor.h"
-#include "OpenDoor.generated.h"
+#include "Grabber.generated.h"
 
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
-class BUILDINGESCAPEPR_API UOpenDoor : public UActorComponent
+class BUILDINGESCAPEPR_API UGrabber : public UActorComponent
 {
 	GENERATED_BODY()
 
 public:	
 	// Sets default values for this component's properties
-	UOpenDoor();
+	UGrabber();
 
 protected:
 	// Called when the game starts
 	virtual void BeginPlay() override;
 
-	void OpenDoor();
-	void CloseDoor();
-
 public:	
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
-
-private:
-	UPROPERTY(EditAnywhere)
-	float OpenAngle = 90.f;
-
-	AActor* Owner;
-
-	UPROPERTY(EditAnywhere)
-	ATriggerVolume* PressurePlate;
+		
 	
-	float DoorCloseDelay = 1.f;
-
-	
-	float doorLastOpenTime;
-
-	AActor* ActorThatOpens; // Remember pawn inherits from Actor
-
-	//
-
 };
